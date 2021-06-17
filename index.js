9d12d5d3a731ffda38a893ada10b61ad2dfd8ba1
vchavarria1995 <vchavarria1995@gmail.com>
2021-06-16 19:39:49 -0500

    finished task 3, missed something in task 2 going back to finishing it

--- index.js
+++ index.js
@@ -92,9 +92,15 @@ function Airplane(name) {
           + Should return a string "Playing with x", x being the favorite toy.
   */
  function Baby(name, age, favoriteToy) {
-   
+   Person.call(this, name, age);
+   this.favoriteToy = favoriteToy;
   }
+
+  Baby.prototype = Object.create(Person.prototype);
  
+  Baby.prototype.play = function(){
+    return `Playing with ${this.favoriteToy}`;
+  }
   
   /* 
     TASK 4
